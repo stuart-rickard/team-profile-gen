79e8d77d505aa5132bd735dbf9ccc382ae71e9a9
stuart-rickard <stuart@bau-dev.com>
2022-01-09 09:48:26 -0800

    early outline

---
+++ __tests__/obtain-team-info.test.js
@@ -0,0 +1,10 @@
+// const { expect } = require('@jest/globals');
+// const { toByteArray } = require('base64-js');
+// const obtainTeamInfo = require('../lib/obtain-team-info.js');
+
+test('checks for object output', () => {
+    expect(typeof({})).toBe("object");
+});
+
+
+
---
+++ app.js
@@ -0,0 +1,7 @@
+const Team = require( './lib/Team' );
+
+new Team().introMessage
+.then.getTeamMembers
+.then.createHTML
+.then.saveHTML
+.then.openHTML
